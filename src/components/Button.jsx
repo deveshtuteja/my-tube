da04f05e3fdc2842162aee5c4d12148ad32f324f
deveshtuteja <deveshtuteja17@gmail.com>
2024-08-18 23:23:56 +0530

    button list

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ({ name }) => {
+  return <button className="px-4 m-2 bg-gray-200 rounded-lg">{name}</button>;
+};
+
+export default Button;
--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,5 +1,18 @@
+import Button from "./Button";
+
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className="flex">
+      <Button name="All" />
+      <Button name="Gaming" />
+      <Button name="Live" />
+      <Button name="Cricket" />
+      <Button name="Coding" />
+      <Button name="Music" />
+      <Button name="Cricket" />
+      <Button name="Mixes" />
+    </div>
+  );
 };
 
 export default ButtonList;
