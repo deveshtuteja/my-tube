c2710ae306be0dae27848556171e2dd275aab9d7
deveshtuteja <deveshtuteja17@gmail.com>
2024-08-21 13:01:41 +0530

    made videos page ui better

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,3 +1,5 @@
+import { useSelector } from "react-redux";
+
 const VideoCard = ({ info }) => {
   console.log(info);
   if (!info) {
@@ -6,6 +8,23 @@ const VideoCard = ({ info }) => {
   }
   const { snippet, statistics } = info;
   const { title, channelTitle, thumbnails } = snippet;
+
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+  if (!isMenuOpen)
+    return (
+      <div className="py-2 px-4 m-2 w-[286px] shadow-md hover:shadow-xl bg-center">
+        <img
+          src={thumbnails.medium.url}
+          alt="thumbnail"
+          className="rounded-xl"
+        />
+        <ul>
+          <li className="font-bold py-1">{title}</li>
+          <li>{channelTitle}</li>
+          <li>{statistics.viewCount} views</li>
+        </ul>
+      </div>
+    );
   return (
     <div className="py-2 px-4 m-2 w-[264px] shadow-md hover:shadow-xl bg-center">
       <img src={thumbnails.medium.url} alt="thumbnail" className="rounded-xl" />
